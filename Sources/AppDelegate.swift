dd5fdedc44ce29acf113a41fe56fb618ea3cd931
Anvipo <anvipo.apps@gmail.com>
2021-09-05 09:46:15 +0300

    Initial Commit

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  Pomogator
+//
+//  Created by Anvipo on 05.09.2021.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder {}
+
+extension AppDelegate: UIApplicationDelegate {}
